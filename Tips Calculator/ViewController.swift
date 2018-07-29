a9bf435eafcaf6d1eed40d102855ebc7e952bc3a
Hsuen-Ju Li <lhj@hsuen-jude-macbook-pro.local>
2018-07-30 00:30:01 +0800

    Initial Commit

---
+++ Tips Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tips Calculator
+//
+//  Created by Hsuen-Ju Li on 2018/7/30.
+//  Copyright © 2018年 Hsuen-Ju Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
