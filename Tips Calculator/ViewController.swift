829b8427e8303e42b029779434b2680dd0e16c32
Hsuen-Ju Li <lhj@2001-b400-e252-da27-0c23-3ccd-18c4-0adc.emome-ip6.hinet.net>
2018-07-30 01:44:07 +0800

    Complete tips calculator

--- Tips Calculator/ViewController.swift
+++ Tips Calculator/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet var priceTextField : UITextField!
+    @IBOutlet var percentageTextField : UITextField!
+    @IBOutlet var totalPrice : UILabel!
+    @IBOutlet var peopleShareTextField : UITextField!
+    @IBOutlet var sharePriceLabel : UILabel!
+    
+    
+    //Variables
+    var price = 0.0
+    var percentage = 0.0
+    var total = 0.0
+    var people = 0
+    var sharePrice = 0.0
+    
+    func priceInit(){
+        price = 0.0
+        percentage = 0.0
+        total = 0.0
+        people = 0
+        sharePrice = 0.0
+        priceTextField.text = ""
+        percentageTextField.text = ""
+        totalPrice.text = String(total)
+        peopleShareTextField.text = ""
+        sharePriceLabel.text = String(sharePrice)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        priceInit()
     }
 
+    @IBAction func countBtnPressed(_ sender : UIButton){
+        if percentageTextField != nil && priceTextField != nil{
+            price = Double(priceTextField.text!)!
+            percentage = Double(percentageTextField.text!)!
+            calTotalPrice(price: price, percentage: percentage)
+        }
+        
+    }
 
+    @IBAction func sharePeopleInput(_ sender: Any) {
+        self.resignFirstResponder()
+        sharePrice = total / Double(peopleShareTextField.text!)!
+        sharePriceLabel.text = String(format: "%.2f", sharePrice)
+    }
+    
+    func calTotalPrice(price : Double, percentage : Double){
+        total = price + (price * (percentage / 100))
+        totalPrice.text = String(format: "%.2f", total)
+    }
+    
+    @IBAction func clearBtnPressed(_ sender : UIButton){
+        priceInit()
+    }
+    
+    
 }
 
